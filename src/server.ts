cb8b4ce6ca11256e67bf0ec3a0ed79f1c9c7ae07
Marcel <roxsublime@gmail.com>
2023-07-04 12:05:44 -0300

    att11

--- src/server.ts
+++ src/server.ts
@@ -676,6 +676,12 @@ async function bootstrap(){
             
 
 
-    await fastify.listen({port:3333})
+    await fastify.listen({
+      host:'0.0.0.0',
+      port: process.env.PORT ? Number(process.env.PORT) : 3333,
+
+    }).then(() => {
+      console.log('HTTP is Running')
+    })
 }
 bootstrap()
